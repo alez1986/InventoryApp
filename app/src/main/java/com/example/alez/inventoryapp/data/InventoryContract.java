2431a927fc45e593cb31aa6c975ad9378d99299e
Alexander Giss <alez1986@gmail.com>
2018-07-17 19:25:37 +0300

    Added contract and db helper classes

--- app/src/main/java/com/example/alez/inventoryapp/data/InventoryContract.java
+++ app/src/main/java/com/example/alez/inventoryapp/data/InventoryContract.java
@@ -1,5 +1,51 @@
 package com.example.alez.inventoryapp.data;
 
+import android.provider.BaseColumns;
+
 
 public class InventoryContract {
+
+    private InventoryContract() {}
+
+    public static final class InventoryEntry implements BaseColumns {
+
+        /** Name of database table for products */
+        public final static String TABLE_NAME = "product_inventory";
+
+        /**
+         * Unique ID number for the product (only for use in the database table).
+         * Type: INTEGER
+         */
+        public final static String _ID = BaseColumns._ID;
+
+        /**
+         * Name of the product.
+         * Type: TEXT
+         */
+        public final static String COLUMN_PRODUCT_NAME ="product_name";
+
+        /**
+         * Price of the product.
+         * Type: INTEGER
+         */
+        public final static String COLUMN_PRODUCT_PRICE = "price";
+
+        /**
+         * Quantity of the product.
+         * Type: INTEGER
+         */
+        public final static String COLUMN_PRODUCT_QUANTITY = "quantity";
+
+        /**
+         * Supplier name of the product.
+         * Type: TEXT
+         */
+        public final static String COLUMN_PRODUCT_SUPPLIER_NAME = "supplier_name";
+
+        /**
+         * Supplier phone of the product.
+         * Type: TEXT
+         */
+        public final static String COLUMN_PRODUCT_SUPPLIER_PHONE_NUMBER = "supplier_phone_number";
+    }
 }
